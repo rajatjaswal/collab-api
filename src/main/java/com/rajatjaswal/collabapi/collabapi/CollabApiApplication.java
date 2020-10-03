3d3c2ce15bc0eee789053c66b9df016e7921acb5
rajatjaswal <44356753+rajatjaswal@users.noreply.github.com>
2020-10-03 14:39:33 -0400

    Init commit

---
+++ src/main/java/com/rajatjaswal/collabapi/collabapi/CollabApiApplication.java
@@ -0,0 +1,13 @@
+package com.rajatjaswal.collabapi.collabapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollabApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rajatjaswal/collabapi/collabapi/CollabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajatjaswal.collabapi.collabapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
